e8599d4055faec6545c568f462543a016b53f236
Luiz Gustavo <luiz.gustavo.costa82@gmail.com>
2019-06-24 00:51:17 +0200

    Refactor log

---
+++ numbers/src/test/java/com/guga/numbers/AllNumbersTest.java
@@ -0,0 +1,32 @@
+package com.guga.numbers;
+
+import org.assertj.core.api.WithAssertions;
+import org.junit.Test;
+
+import java.util.function.Function;
+
+
+public class AllNumbersTest implements WithAssertions {
+
+    private Function<String, String> functionDescription = s -> "Instance of "+s+" class";
+
+    @Test
+    public void givenAPrimitiveWhenCheckTheInstanceThenReturnTheWrapperClass() {
+
+        int myFirstInt = 42;
+        assertThat(myFirstInt).as(functionDescription.apply("Integer")).isInstanceOf(Integer.class);
+
+        float myFirstFloat = 42;
+        assertThat(myFirstFloat).as(functionDescription.apply("Float")).isInstanceOf(Float.class);
+
+        double myFirstDouble = 42;
+        assertThat(myFirstDouble).as(functionDescription.apply("Double")).isInstanceOf(Double.class);
+
+        boolean myFirstBoolean = false;
+        assertThat(myFirstBoolean).as(functionDescription.apply("Boolean")).isInstanceOf(Boolean.class);
+
+    }
+
+
+
+}
